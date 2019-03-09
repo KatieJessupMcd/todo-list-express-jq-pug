ece3df2831e28663c2aadd2338ebcf82714f670d
KatieJessupMcD <katie.jessupmcd@gmail.com>
2019-03-09 14:08:53 -0800

    added functionality to jq

--- public/list.js
+++ public/list.js
@@ -1,3 +1,4 @@
+// List Jquery methods to handle form data, handle and process add/delete
 $(document).ready(function() {
   // processForm: get data from form and make AJAX call to our API
   function processForm(evt) {
@@ -26,35 +27,83 @@ $(document).ready(function() {
     if (!resp.errors) {
       let listItemToAdd = resp['new_list']['list_item'];
       let listItemId = resp['new_list']['id'];
-      console.log('This is List item id from handleResponse ', listItemId);
+      let $deleteIcon = $('<i>')
+        .addClass('far fa-trash-alt')
+        .css({ color: 'Tomato' })
+        .attr('id', 'delete');
+      let $updateIcon = $('<i>')
+        .addClass('far fa-edit')
+        .css({ color: 'Mediumslateblue' })
+        .attr('id', 'update');
+      let $listItem = listItemToAdd;
+      let $completeListItem = $('<li>')
+        .append($deleteIcon)
+        .append($updateIcon)
+        .append($listItem)
+        .attr('id', listItemId)
+        .addClass(
+          'list-group-item d-flex justify-content-between align-items-center'
+        );
+
       // append listItemToAdd to DOM along with corresponding db id as an attr, dynamically create a new list
-      $('ul').append(
-        $('<li>')
-          .text(listItemToAdd)
-          .attr('id', listItemId)
-      );
+      $completeListItem.prependTo($('ol'));
+
       $('#form').trigger('reset');
     }
   }
 
   $('#form').on('submit', processForm);
 
+  // *******PROCESS UPDATE TODO
+  function processUpdate(evt) {
+    evt.preventDefault();
+    // grab clicked on li, and grab is associated id attr
+    let clickedList = $(evt.target).parent();
+    let clickedListText = clickedList.text();
+    let clickedListId = clickedList.attr('id');
+    console.log('this is the clickedListId from UPDATE ', clickedListId);
+
+    // format into a JSON friendly obj to send in ajax req
+    let updateDataId = {
+      id: clickedListId,
+      list_item: clickedListText
+    };
+
+    // PATCH ajax request to api
+
+    $.ajax({
+      url: 'http://localhost:3000/lists',
+      method: 'PATCH',
+      data: JSON.stringify(updateDataId),
+      dataType: 'json',
+      contentType: 'application/json',
+      success: updateItem
+    });
+  }
+
+  function updateItem(resp) {
+    // if the ajax req is sucessful, update dom by updateing li using item id from response
+    if (!resp.errors) {
+      $('.alert')
+        .fadeIn()
+        .delay(2000)
+        .text(resp.message)
+        .fadeOut();
+    }
+  }
+
+  $('ol').on('click', '#update', processUpdate);
+
   // deleteItem: make AJAX request to delete list item on click
 
   function processDelete(evt) {
     evt.preventDefault();
-    let clickedList = event.target;
-    console.log(clickedList);
+    // grab clicked on li, and grab is associated id attr
+    let clickedList = evt.target.parentElement;
+    console.log('this is the parent of the evt.target ', clickedList);
     let clickedListId = clickedList.getAttribute('id');
 
-    console.log('this is the event.target ', event.target);
-
-    console.log('this is the clickedListId ', clickedListId);
-
-    // TODO!!!! PASS IN ID OF CLICKED LI TO AJAX REQUEST - PERHAPS
-    // A HELPER FUNCTION TO GRAB CLICKED ELEMENT ID ATTR !!!!
-
-    // grab item to delete data
+    // format into a JSON friendly obj to send in ajax req
     let deleteDataId = {
       id: clickedListId
     };
@@ -72,15 +121,16 @@ $(document).ready(function() {
   }
 
   function deleteItem(resp) {
+    // if the ajax req is sucessful, update dom by removing deleted li using item id from response
     if (!resp.errors) {
-      // this will remove the list from the DOM- ONCE YOU KNOW IT IS SUCESSFUL- make this a on sucess function from ajax delete req
-      $('li').on('click', function() {
-        $(this)
-          .closest('li')
-          .remove();
-      });
+      $("li[id='" + resp.id + "']").remove();
+      $('.alert')
+        .fadeIn()
+        .delay(2000)
+        .text(resp.message)
+        .fadeOut();
     }
   }
 
-  $('li').on('click', processDelete);
+  $('ol').on('click', '#delete', processDelete);
 }); // end of window
